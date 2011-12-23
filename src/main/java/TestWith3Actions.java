8f935965d381f784b57c2f1b8abb669c1bdcd206
Ivan Shubin <ivan.ishubin@gmail.com>
2011-12-23 14:10:33 +0100

    Added new test

---
+++ src/main/java/TestWith3Actions.java
@@ -0,0 +1,23 @@
+import net.mindengine.oculus.experior.annotations.Action;
+import net.mindengine.oculus.experior.annotations.Test;
+
+
+
+@Test(name="Test With 3 Actions", project="sample-project")
+public class TestWith3Actions {
+
+    @Action(name="Action 1", next="action2")
+    public void action1() {
+        System.out.println("action 1");
+    }
+    
+    @Action(name="Action 2", next="action3")
+    public void action2() {
+        System.out.println("action 2");
+    }
+    
+    @Action(name="Action 3")
+    public void action3() {
+        System.out.println("action 3");
+    }
+}
